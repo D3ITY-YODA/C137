250a2b9b685189171b73839189e87f8726252f3f
evajuma <evansjuma1e@gmail.com>
2026-05-29 01:52:05 +0300

    feat: add types for Donation, Allocation, and NGOTransparencySDKConfig

---
+++ uwaziSDK/sdk/src/types.ts
@@ -0,0 +1,20 @@
+export interface Donation {
+  donor: string;
+  amount: bigint;
+  timestamp: bigint;
+}
+
+export interface Allocation {
+  beneficiary: string;
+  amount: bigint;
+  timestamp: bigint;
+  confirmed: boolean;
+}
+
+import type { Eip1193Provider, InterfaceAbi } from "ethers";
+
+export interface NGOTransparencySDKConfig {
+  contractAddress: string;
+  abi: InterfaceAbi;
+  provider: Eip1193Provider;
+}
