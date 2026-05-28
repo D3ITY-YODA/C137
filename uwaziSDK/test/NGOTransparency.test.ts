2fae5558a8705573cb0461c76328533d925477f6
bfrancis <brendanakudo@gmail.com>
2026-05-29 01:14:15 +0300

    test: add NGOTransparency deployment tests

--- uwaziSDK/test/NGOTransparency.test.ts
+++ uwaziSDK/test/NGOTransparency.test.ts
@@ -1,3 +1,16 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { NGOTransparency } from "../typechain-types";
+
+describe("NGOTransparency", () => {
+  let contract: NGOTransparency;
+  let admin: Awaited<ReturnType<typeof ethers.getSigners>>[0];
+  let donor: Awaited<ReturnType<typeof ethers.getSigners>>[1];
+  let beneficiary: Awaited<ReturnType<typeof ethers.getSigners>>[2];
+
+  beforeEach(async () => {
+    [admin, donor, beneficiary] = await ethers.getSigners();
+    const factory = await ethers.getContractFactory("NGOTransparency");
+    contract = await factory.connect(admin).deploy();
+    await contract.waitForDeployment();
+  });
